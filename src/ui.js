7ea2b8e6f960a8ac0c84c1dd1f1274923633c3cb
Sara Magdits <saramagdits@gmail.com>
2018-11-05 17:48:12 -0500

    Added initial card styling for recipes. Added badges for health labels.

--- src/ui.js
+++ src/ui.js
@@ -11,14 +11,15 @@ class UI {
 	}
 
 	// Receive produce results and display them on the page as clickable tiles
-	displayProduce (produce) {
+	displayProduce(produce) {
 		let content = '';
 		produce.produce.forEach(produceItem => {
-			content += `<li><a href='#' class='produce-link' data-name='${produceItem}'>${produceItem}<img src='images/${produceItem.replace(/\s/g, "")}.jpg'></a></li>`;
+			content += `<li><a href='#' class='produce-link' data-name='${produceItem}'>${produceItem}<img src='images/${produceItem.replace(/\s/g, '')}.jpg'></a></li>`;
 		});
 		this.produceList.innerHTML = content;
 		// console.log(produce);
 	}
+
 	//  Give the produce styling to signify it has been selected
 	// addSelectedStyling (produce) {
 	// 	produce.classList.add('selected');
@@ -27,47 +28,59 @@ class UI {
 	// removeSelectedStyling (produce) {
 	// 	produce.classList.remove('selected');
 	// }
-	toggleSelected (produce) {
+	toggleSelected(produce) {
 		produce.classList.toggle('selected');
 	}
-	removeAllSelectedStyling () {
-		document.querySelectorAll('.selected').forEach(produce => {produce.classList.remove('selected')});
+
+	removeAllSelectedStyling() {
+		document.querySelectorAll('.selected').forEach(produce => {
+			produce.classList.remove('selected')
+		});
 	}
 
 	// Receive recipe results and display them on the page as cards
-	displayRecipes (recipes) {
+	displayRecipes(recipes) {
 		let content = '';
 		recipes.hits.forEach(recipe => {
-			content += `<li>
-										<a href='${recipe.recipe.url}'>${recipe.recipe.label}</a>
-										<i>${recipe.recipe.source}</i>
-										<img src='${recipe.recipe.image}'>
-									</li>`;
+			let badges = '';
+			badges += recipe.recipe.healthLabels.map(label => {
+				return `<span class='badge badge-info'>${label}</span>`
+			}).reduce((a, b) => a + b);
+			content += `<div class='card recipe-card'>
+  									<img class='card-img-top' src='${recipe.recipe.image}' alt='${recipe.recipe.label}'>
+										<div class='card-body'>
+											<h5 class='card-title'>${recipe.recipe.label}</h5>
+											<p class='card-text'>${badges}</p>
+											<a href='${recipe.recipe.url}' class='btn btn-primary'>${recipe.recipe.source}</a>
+										</div>
+									</div>`;
 		});
 		this.recipeList.innerHTML = content;
 	}
 
 	// Display a success or error alert, with custom text
-	showAlert (alertMsg, type) {
+	showAlert(alertMsg, type) {
 		if (type === 'success') {
 			let content = document.createElement('div');
-			content.classList.add('alert','alert-success');
+			content.classList.add('alert', 'alert-success');
 			content.role = 'alert';
 			content.innerHTML = alertMsg;
 			this.recipeContainer.insertBefore(content, this.recipeList);
 		}
 		else if (type === 'error') {
 			let content = document.createElement('div');
-			content.classList.add('alert','alert-danger');
+			content.classList.add('alert', 'alert-danger');
 			content.role = 'alert';
 			content.innerHTML = alertMsg;
 			this.recipeContainer.insertBefore(content, this.recipeList);
 		}
 	}
-	clearAlerts () {
+
+	clearAlerts() {
 		document.querySelectorAll('.alert').forEach(alert => alert.remove());
 	}
-	clearRecipes () {
+
+	clearRecipes() {
 		while (this.recipeList.firstChild) {
 			this.recipeList.removeChild(this.recipeList.firstChild);
 		}
